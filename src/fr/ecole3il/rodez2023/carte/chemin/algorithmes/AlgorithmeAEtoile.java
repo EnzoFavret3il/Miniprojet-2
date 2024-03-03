d3335759373cb4772e038161f194dea8737db548
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-03-03 12:07:46 +0100

    [JAVACODE] methode trouverChemin=> OK

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeAEtoile.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeAEtoile.java
@@ -3,6 +3,7 @@ package fr.ecole3il.rodez2023.carte.chemin.algorithmes;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -12,11 +13,39 @@ import fr.ecole3il.rodez2023.carte.chemin.elements.Noeud;
 
 public class AlgorithmeAEtoile<E> implements AlgorithmeChemin<E> {
 
+	//Methode qui trouve le chemin good
 	@Override
-	public List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	/**@param graphe   le graphe pour la recherche
+	 *@param depart   le nœud de départ
+	 *@param arrivee  le nœud d'arrivée
+	 *@return liste de noeud représentant le chemin le plus court eentre depart/arrivee
+	 */
+   public List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee) {
+       Map<Noeud<E>, Double> coutsEstimes = initialiserCoutsEstimes(graphe, depart, arrivee);
+       Map<Noeud<E>, Double> coutsActuels = new HashMap<>();
+       Map<Noeud<E>, Noeud<E>> predecesseurs = new HashMap<>();
+       PriorityQueue<Noeud<E>> filePriorite = initialiserFilePriorite(coutsEstimes);
+
+       coutsActuels.put(depart, 0.0);
+
+       while (!filePriorite.isEmpty()) {
+           Noeud<E> noeudActuel = filePriorite.poll();
+           if (noeudActuel.equals(arrivee)) {
+               break;
+           }
+           for (Noeud<E> voisin : graphe.getVoisins(noeudActuel)) {
+               double nouveauCout = coutsActuels.get(noeudActuel) + graphe.getCoutArete(noeudActuel, voisin);
+               if (!coutsActuels.containsKey(voisin) || nouveauCout < coutsActuels.get(voisin)) {
+                   coutsActuels.put(voisin, nouveauCout);
+                   double coutTotalEstime = nouveauCout + coutsEstimes.get(voisin);
+                   filePriorite.add(voisin);
+                   predecesseurs.put(voisin, noeudActuel);
+               }
+           }
+       }
+
+       return construireChemin(arrivee, predecesseurs);
+   }
 	private Map<Noeud<E>, Double> initialiserCoutsEstimes(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee) {
 	
 	}
