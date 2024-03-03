eb2a41826da0506f6f82c2c23539e2042554f182
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-03-03 10:15:22 +0100

    [JAVACODE] test unitaire pour algoChemin

---
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgoCheminTest.java
@@ -0,0 +1,14 @@
+package fr.ecole3il.rodez2023.carte.chemin.algorithmes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AlgoCheminTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
