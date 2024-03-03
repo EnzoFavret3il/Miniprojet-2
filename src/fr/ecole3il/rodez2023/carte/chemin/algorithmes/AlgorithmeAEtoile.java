014a253e8b839a875f58b08eca028abe29896c04
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-03-03 11:55:07 +0100

    [JAVACODE] Ajout Méthodes

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeAEtoile.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeAEtoile.java
@@ -1,6 +1,11 @@
 package fr.ecole3il.rodez2023.carte.chemin.algorithmes;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 import fr.ecole3il.rodez2023.carte.chemin.elements.Graphe;
 import fr.ecole3il.rodez2023.carte.chemin.elements.Noeud;
@@ -18,4 +23,28 @@ public class AlgorithmeAEtoile<E> implements AlgorithmeChemin<E> {
 	private double calculerDistanceCuiCui(Noeud<E> noeud1, Noeud<E> noeud2) {
 		
 	}
+	/**Initialisation Priorité
+	*@param coutsEstimes une map contenant les coûts estimés pour chaque nœud
+    * @return une file de priorité initialisée avec les coûts estimés des nœuds
+    */
+   private PriorityQueue<Noeud<E>> initialiserFilePriorite(Map<Noeud<E>, Double> coutsEstimes) {
+       return new PriorityQueue<>(Comparator.comparingDouble(coutsEstimes::get));
+   }
+   
+ //Methode de contruction du chemin en fonction du chemin le plus court
+   /**@param arrivee        le nœud d'arrivee
+    *@param predecesseurs  une map contenant les prédécesseurs de chaque nœud
+    *@return une liste de nœuds représentant le chemin le plus court
+    */
+   private List<Noeud<E>> construireChemin(Noeud<E> arrivee, Map<Noeud<E>, Noeud<E>> predecesseurs) {
+       List<Noeud<E>> chemin = new ArrayList<>();
+       Noeud<E> noeud = arrivee;
+       while (noeud != null) {
+           chemin.add(noeud);
+           noeud = predecesseurs.get(noeud);
+       }
+       //inversion de la collection pour récupérer le chemin dans le bonne ordre
+       Collections.reverse(chemin);
+       return chemin;
+   }
 }
