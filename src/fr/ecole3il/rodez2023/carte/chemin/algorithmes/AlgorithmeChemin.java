b533f9b3bf8c366400ed857fa9093120454d5fd1
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-03-03 10:03:50 +0100

    [JAVDOC] Ajout JavaDoc AlgorithmeChemin

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeChemin.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeChemin.java
@@ -4,7 +4,13 @@ import fr.ecole3il.rodez2023.carte.chemin.elements.Graphe;
 import fr.ecole3il.rodez2023.carte.chemin.elements.Noeud;
 
 import java.util.List;
-
+/**@param <E> type générique des val du graphe*/
 public interface AlgorithmeChemin<E> {
+	/**@param => dans l'order:
+	 * -	Graphe<E> graphe => graphe où la recherche est effectué
+	 * -	Noeud<E> départ => noeud de départ
+	 * -	Noeud<E> arrivee => noeud d'arrive
+	 * 
+	 * @return => renvoie un liste de noeud permettant de visualiser le chemin*/
     List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee);
 }
