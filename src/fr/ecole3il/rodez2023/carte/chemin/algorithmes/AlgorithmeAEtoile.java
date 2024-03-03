b2867836d0fe89189ec3ebbd0dc57b6d7f62932b
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-03-03 11:52:18 +0100

    [JAVACODE] Ecriture Methodes

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeAEtoile.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeAEtoile.java
@@ -1,5 +1,21 @@
 package fr.ecole3il.rodez2023.carte.chemin.algorithmes;
 
-public class AlgorithmeAEtoile {
+import java.util.List;
 
+import fr.ecole3il.rodez2023.carte.chemin.elements.Graphe;
+import fr.ecole3il.rodez2023.carte.chemin.elements.Noeud;
+
+public class AlgorithmeAEtoile<E> implements AlgorithmeChemin<E> {
+
+	@Override
+	public List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private Map<Noeud<E>, Double> initialiserCoutsEstimes(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee) {
+	
+	}
+	private double calculerDistanceCuiCui(Noeud<E> noeud1, Noeud<E> noeud2) {
+		
+	}
 }
