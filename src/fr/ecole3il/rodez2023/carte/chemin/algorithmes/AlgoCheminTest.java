bfd731fa29a8abf554233f2714e34499cf093a9a
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-03-03 10:21:07 +0100

    [JAVACODE] Debut code tests

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgoCheminTest.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgoCheminTest.java
@@ -1,14 +1,24 @@
 package fr.ecole3il.rodez2023.carte.chemin.algorithmes;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import fr.ecole3il.rodez2023.carte.chemin.algorithmes.AlgorithmeChemin;
+import fr.ecole3il.rodez2023.carte.chemin.elements.Graphe;
+import fr.ecole3il.rodez2023.carte.chemin.elements.Noeud;
+import java.util.List;
+
 
 class AlgoCheminTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testFindDaWay() {
+		//Graphe fictif pour les simuler les tests
+		Graphe<String> graphe = new Graphe<>();
+        Noeud<String> depart = new Noeud<>("Bar");
+        Noeud<String> arrivee = new Noeud<>("Maison");
+        graphe.ajouterNoeud(depart);
+        graphe.ajouterNoeud(arrivee);
+        graphe.ajouterArete(depart, arrivee, 1.0);
 	}
 
 }
